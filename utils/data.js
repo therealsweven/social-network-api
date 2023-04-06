601b5c6720011b02ad603768e329bdcd396d564f
therealsweven <robssimpson11@gmail.com>
2023-04-06 17:39:13 -0600

    Seeds and all User routes working properly

---
+++ utils/data.js
@@ -0,0 +1,41 @@
+// for demo
+// {
+// 	"username": "tommyboy819",
+// 	"email": "tommy@gmail.com"
+// }
+
+const users = [
+  {
+    username: "djbobs",
+    email: "bob@gmail.com",
+  },
+  {
+    username: "sean123",
+    email: "sean@gmail.com",
+  },
+  {
+    username: "conner4321",
+    email: "conner@gmail.com",
+  },
+  {
+    username: "jake321",
+    email: "jake@gmail.com",
+  },
+];
+
+const thoughts = [
+  {
+    thoughtText: "Testing TESTING TESTING",
+    username: "djbobs",
+  },
+  {
+    thoughtText: "nldbcalsdjbclaksburkhbjadsfadsf",
+    username: "sean123",
+  },
+  {
+    thoughtText: "qwertyqwertyqwertyqwertyqwerty",
+    username: "jake321",
+  },
+];
+
+module.exports = { users, thoughts };
---
+++ utils/seed.js
@@ -0,0 +1,28 @@
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { users, thoughts } = require("./data");
+console.log(users);
+console.log(thoughts);
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected");
+
+  // Drop existing courses
+  await User.deleteMany({});
+
+  // Drop existing students
+  await Thought.deleteMany({});
+
+  // Add users to the collection and await the results
+  await User.collection.insertMany(users);
+
+  // Add thoughts to the collection and await the results
+  await Thought.collection.insertMany(thoughts);
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(users);
+  console.table(thoughts);
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
